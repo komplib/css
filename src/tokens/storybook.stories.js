bc423a497cf9935b754896dcfb0832fc694d816f
Luiz Tanure <letanure@gmail.com>
2022-06-07 11:47:16 +0200

    test: start design token storybook storie

---
+++ src/tokens/storybook.stories.js
@@ -0,0 +1,20 @@
+import tokens from "./.tokens.json";
+
+export default {
+  title: "Tokens",
+};
+
+const Template = ({ name, group, ...args }) => {
+  console.log(group);
+  return `
+    <div>${name}</div>
+    <div>${group.value}</div>
+    <div>${group.type}</div>
+    `;
+};
+
+export const FontFamily = Template.bind({});
+FontFamily.args = {
+  name: "primary",
+  group: tokens.font.family.primary,
+};
