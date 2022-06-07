962fb48f28dad0b18b1f85033c5f0a271a31bfbc
Luiz Tanure <letanure@gmail.com>
2022-06-07 23:39:25 +0200

    docs: update story autoload design tokens

--- src/tokens/storybook.stories.js
+++ src/tokens/storybook.stories.js
@@ -1,20 +1,52 @@
 import tokens from "./tokens.json";
 
+function stringCapitalize(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+function firstLetterToLowercase(str) {
+  return str.charAt(0).toLowerCase() + str.slice(1);
+}
+
+function isObject(obj) {
+  return obj === Object(obj);
+}
+function hasValue(obj) {
+  return obj.value !== undefined;
+}
+
+function tokenPaths(obj, oldPath = "", store = {}, level = 0) {
+  for (let key in obj) {
+    const newPath = `${oldPath}${level > 0 ? stringCapitalize(key) : key}`;
+    if (isObject(obj[key])) {
+      if (hasValue(obj[key])) {
+        store[firstLetterToLowercase(newPath)] = obj[key].value;
+      }
+      tokenPaths(obj[key], newPath, store, ++level);
+    }
+  }
+  return store;
+}
+
 export default {
   title: "Tokens",
 };
 
 const Template = ({ name, group, ...args }) => {
-  console.log(group);
   return `
-    <div>${name}</div>
-    <div>${group.value}</div>
-    <div>${group.type}</div>
-    `;
+    <ul>
+      ${Object.entries(group)
+        .map(([key, value]) => {
+          return `
+          <li>
+            --${key}: ${value};
+          </li>`;
+        })
+        .join("")}
+    </ul>`;
 };
 
 export const FontFamily = Template.bind({});
 FontFamily.args = {
-  name: "primary",
-  group: tokens.font.family.primary,
+  group: tokenPaths(tokens),
 };
