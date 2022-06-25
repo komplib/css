6ff4016142fe0ab6a63d49a94c3544672c8f26e4
Luiz Tanure <letanure@gmail.com>
2022-06-25 14:38:59 +0200

    refactor(style): update token vars

---
+++ src/tokens/capitalize.js
@@ -0,0 +1,22 @@
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+module.exports = function () {
+  return function (stylus) {
+    stylus.define("capitalize", function (node) {
+      var nodeName = node.nodeName,
+        val = node.string;
+
+      if ("string" == nodeName) {
+        return new stylus.nodes.String(capitalizeFirstLetter(val));
+      } else if ("ident" == nodeName) {
+        return new stylus.nodes.Ident(capitalizeFirstLetter(val));
+      } else {
+        throw new Error(
+          'capitalize accepts string or ident but got "' + nodeName + '"'
+        );
+      }
+    });
+  };
+};
